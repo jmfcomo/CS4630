27f872fad9b45800ec9f2b20eb03d3bc29513401
Ferguson <jfr48@umsystem.edu>
2024-10-30 14:41:34 -0500

    shmup enemies 2 and 3

--- Assets/_Scripts/Enemy.cs
+++ Assets/_Scripts/Enemy.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Scripting.APIUpdating;
 
+[RequireComponent(typeof(BoundsCheck))]
 public class Enemy : MonoBehaviour
 {
 
@@ -12,7 +13,7 @@ public class Enemy : MonoBehaviour
     public float health = 10;
     public int score = 100;
 
-    private BoundsCheck bndCheck;
+    protected BoundsCheck bndCheck;
 
     private void Awake()
     {
